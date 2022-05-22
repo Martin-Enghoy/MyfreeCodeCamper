1255e740f7432010717b2c9040848572488ac51e
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-22 21:32:49 +0800

    Initialized jS Objects Module.

---
+++ javaScript-Algorithms-and-Data-Structures/jS-objects1.js
@@ -0,0 +1,22 @@
+//  JAVASCRIPT OBJECTS WOOOOOOOO
+//       1.) If your object has any non-string properties, JavaScript will automatically typecast them as strings.
+
+const myDog = {
+    name: "Sky",
+    legs: 4,
+    tails: 1,
+    friends: ["Star", "Sunny", "Cloud", "Summer", "Storm"]
+};
+
+
+/////////////////////////////////////////////////////
+
+// CALLING VALUES IN OBJECT USING DOT (.) NOTATION
+const testObj = {
+    "hat": "ballcap",
+    "shirt": "jersey",
+    "shoes": "cleats"
+};
+
+const hatValue = testObj.hat;    
+const shirtValue = testObj.shirt;   
