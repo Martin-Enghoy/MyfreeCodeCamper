65aa3b27bb5df1d9898f9da3d3730adb073ca09c
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-25 20:53:21 +0800

    Finished jS Ternary Operators.

--- javaScript-Algorithms-and-Data-Structures/Basics Module/Basic-Syntax/basic-javaScript7.js
+++ javaScript-Algorithms-and-Data-Structures/Basics Module/Basic-Syntax/basic-javaScript7.js
@@ -1,4 +1,4 @@
-//  parses a string and returns an integer
+//  Parses a string and returns an integer
 //      If the first character in the string can't be converted into a number, then it returns NaN.
 function convertToInteger(str) {
     str = parseInt(str);
@@ -9,7 +9,7 @@ convertToInteger("56");
 
 /////////////////////////////////////////////////////
 
-//  parses a string and returns a binary converted from an integer
+//  Parses a string and returns a binary converted from an integer
 function convertToBinary(str) {
     str = parseInt(str, 2);
 
@@ -18,3 +18,22 @@ function convertToBinary(str) {
 
 convertToBinary("10011");
 
+/////////////////////////////////////////////////////
+
+//  Ternary Operator, converts a single if-else to a one-liner return.
+function checkEqual(a, b) {
+    return a === b ? "Equal" : "Not Equal";
+}
+
+checkEqual(1, 2);
+
+/////////////////////////////////////////////////////
+
+//  Ternary Operator for multiple else-if statements, helps lessen lines of code.
+function checkSign(num) {
+    return (num > 0) ? "positive"
+        : (num < 0) ? "negative"
+            : "zero";
+}
+
+checkSign(10);
