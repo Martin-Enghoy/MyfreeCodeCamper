48c7da5e08149d1975c486672408558a3733f5d0
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-25 20:40:22 +0800

    Finished Integer Parsing.

--- javaScript-Algorithms-and-Data-Structures/Basics Module/Basic-Syntax/basic-javaScript7.js
+++ javaScript-Algorithms-and-Data-Structures/Basics Module/Basic-Syntax/basic-javaScript7.js
@@ -1,3 +1,5 @@
+//  parses a string and returns an integer
+//      If the first character in the string can't be converted into a number, then it returns NaN.
 function convertToInteger(str) {
     str = parseInt(str);
     return str;
@@ -6,3 +8,13 @@ function convertToInteger(str) {
 convertToInteger("56");
 
 /////////////////////////////////////////////////////
+
+//  parses a string and returns a binary converted from an integer
+function convertToBinary(str) {
+    str = parseInt(str, 2);
+
+    return str;
+}
+
+convertToBinary("10011");
+
