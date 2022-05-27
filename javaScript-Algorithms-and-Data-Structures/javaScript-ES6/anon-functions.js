bebe9fa4ccaa44c5c1ac8ff193906b02f62c9382
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-27 09:59:20 +0800

    ES6 - Initialized Arrow Function exercise.

---
+++ javaScript-Algorithms-and-Data-Structures/javaScript-ES6/anon-functions.js
@@ -0,0 +1,4 @@
+// When there is no function body, and only a return value, arrow function syntax 
+// allows you to omit the keyword return as well as the brackets surrounding the code. This helps simplify smaller functions into one-line statements:
+
+const magic = () => new Date();
