7c46a0e6f62d80ed306f9feffc8dfc24933af2bb
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-28 09:32:52 +0800

    javaScript ES6 - Added Spread Operator in Arrays.

--- javaScript-Algorithms-and-Data-Structures/javaScript-ES6/speed.js
+++ javaScript-Algorithms-and-Data-Structures/javaScript-ES6/speed.js
@@ -2,3 +2,10 @@
 //      function sum is able to take any number of arguments and return their sum.
 const sum = (...args) => args.reduce((a, b) => a + b, 0);
 
+//  Spread Operator - unpacks an array for proper copying on new array
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+
+arr2 = [...arr1]; 
+
+console.log(arr2);
