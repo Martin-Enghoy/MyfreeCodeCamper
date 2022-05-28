f58e6d535eed17541ee743c7c9c343a6c5efe7e7
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-28 20:11:55 +0800

    javaScript ES6 - Finished Destructuring Assignment.

--- javaScript-Algorithms-and-Data-Structures/javaScript-ES6/destructuring-Assignment.js
+++ javaScript-Algorithms-and-Data-Structures/javaScript-ES6/destructuring-Assignment.js
@@ -34,3 +34,29 @@ const LOCAL_FORECAST = {
 // const highToday = LOCAL_FORECAST.today.high;
 
 const { today: { low: lowtoday, high: hightoday } } = LOCAL_FORECAST;
+
+
+//      Assign Variables from Arrays
+let a = 8, b = 6;
+[b, a] = [a, b];
+const [e, f, , , c] = [1, 2, 3, 4, 5, 6];
+
+//      Rest Parameter to Reassign Array Elements (when excessive)
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function removeFirstTwo(list) {
+    const [x, y, ...arr] = list;
+    return arr;
+}
+const arr = removeFirstTwo(source);
+
+//      Destrucuting for Function use
+const stats = {
+    max: 56.78,
+    standard_deviation: 4.34,
+    median: 34.54,
+    mode: 23.87,
+    min: -0.75,
+    average: 35.85
+};
+// const half = (stats) => (stats.max + stats.min) / 2.0; 
+const half = ({ max, min }) => (max + min) / 2.0;
