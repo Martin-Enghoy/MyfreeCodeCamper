618b24a5726cbddce57811469ff7a25086eb1bdf
Martin-Enghoy <adrianenghoy@gmail.com>
2022-05-31 20:28:23 +0800

    javaScript Basic Data Structures - Added deepest array.

--- javaScript-Algorithms-and-Data-Structures/Basic-Data-Structures/array.js
+++ javaScript-Algorithms-and-Data-Structures/Basic-Data-Structures/array.js
@@ -94,3 +94,13 @@ function quickCheck(arr, elem) {
 }
 
 console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+
+
+// Deepest Array (5 levels)
+let myNestedArray = [
+    ['unshift', false, 1, 2, 3, 'complex', 'nested'],
+    ['loop', 'shift', 6, 7, 1000, 'method', ['deep']],
+    ['concat', false, true, 'spread', 'array', [['deeper']]],
+    ['mutate', 1327.98, 'splice', 'slice', 'push', [[['deepest']]]],
+    ['iterate', 1.3849, 7, '8.4876', 'arbitrary', 'depth']
+];
