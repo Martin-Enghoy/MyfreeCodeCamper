fdee22d7e64793c831e800c955e9f3030f2a09ae
Martin-Enghoy <adrianenghoy@gmail.com>
2022-06-04 19:18:41 +0800

    javaScript OOP - added more Object functions.

--- javaScript-Algorithms-and-Data-Structures/Object-Oriented/oop.js
+++ javaScript-Algorithms-and-Data-Structures/Object-Oriented/oop.js
@@ -19,3 +19,34 @@ function Dog() {
     this.color = "Brownish White";
     this.numLegs = 4;
 }
+
+let hound = new Dog();
+
+
+// Own Properties check
+let ownProps = [];
+for (let prop in canary) {
+  if (canary.hasOwnProperty(prop)) {
+    ownProps.push(prop);
+  }
+}
+
+// Constructor with params
+// function Dog(name, color) {
+//     this.name = name;
+//     this.color = color;
+//     this.numLegs = 4;
+// }
+
+// let terrier = new Dog("Sky", "brown");
+
+
+// instanceof to check constructor usage
+function House(numBedrooms) {
+    this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House(4);
+myHouse instanceof House;
+
+
