3b7ae01a9abe5127e1f8e8fff94e9dfa3add2734
Martin-Enghoy <adrianenghoy@gmail.com>
2022-08-13 23:35:07 +0800

    Organized Lessons into Folders. Completed Mobile NavBar with Burger Dropdown Menu. Added Assets for Mobile Nav.

---
+++ Responsive-Web-Design/flex-nav/main.js
@@ -0,0 +1,13 @@
+// Nav Bar
+const navBtn = document.querySelector("#menu-btn");
+const nav = document.querySelector("nav");
+const navLinks = document.querySelector(".nav-links");
+
+navBtn.addEventListener("click", () => {
+  navLinks.classList.add("activated");
+  const isExpanded = JSON.parse(navBtn.getAttribute("aria-expanded"));
+  navBtn.setAttribute("aria-expanded", !isExpanded);
+  !isExpanded && nav.classList.add("active");
+});
+
+window.addEventListener("DOMContentLoaded", () => {});
